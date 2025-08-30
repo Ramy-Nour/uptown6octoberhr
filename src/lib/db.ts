a3f6cdc395b39eb28f29fe7f42a0596300614ed1
Ramy-Nour <ramynoureldien@gmail.com>
2025-08-30 18:10:22 +0300

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,8 +1,13 @@
-import { PrismaClient } from '@prisma/client' 
- 
-const globalForPrisma = globalThis as unknown as { 
-} 
- 
-export const db = globalForPrisma.prisma ?? new PrismaClient() 
- 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db 
+import { PrismaClient } from '@prisma/client'
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}
+
+export const db =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ['query'],
+  })
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
